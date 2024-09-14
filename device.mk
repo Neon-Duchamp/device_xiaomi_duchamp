8dfcb32a24112c4bfea2e5cc22e9e6a9ac4e4042
bengris32 <bengris32@protonmail.ch>
2024-09-14 14:50:24 +0100

    duchamp: Kill CarrierConfigOverlay
    * Only seems to cause problems, and we don't really need it as
      we have force enabled IMS features again in https://github.com/Neon-Duchamp/vendor_mediatek_ims/blob/15/ims.mk#L26
    
    Change-Id: Id8ffe30aee1d57ee4b4d61828b605f9da33f1d8c
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- device.mk
+++ device.mk
@@ -240,7 +240,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
 PRODUCT_PACKAGES += \
-   CarrierConfigOverlayDuchamp \
    FrameworksResOverlayDuchamp \
    MiuiCameraOverlayIcon \
    PowerOffAlarmOverlayDuchamp \
