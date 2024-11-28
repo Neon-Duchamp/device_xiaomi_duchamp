755f226c62561ba698f1954d58e9d06dd2f1a054
kenway214 <kenway214@outlook.com>
2024-11-28 16:43:22 +0530

    duchamp: parts: Start parts services as early as possible on bootup
    BOOT_COMPLETED intent is broadcast only after the device is unlocked and user data (encrypted storage) is available.
    Use LOCKED_BOOT_COMPLETED intent for services that don't require accessing data.
    
    Change-Id: I800df08f9c61925ac394f3e6d4861ba68a157797
    Signed-off-by: kenway214 <kenway214@outlook.com>
    Co-Authored-By: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Signed-off-by: UnmoveD <unmoved2121@gmail.com>
    Signed-off-by: zenin1504 <kayozzprojects@yandex.com>

--- parts/src/com/xiaomi/settings/BootCompletedReceiver.java
+++ parts/src/com/xiaomi/settings/BootCompletedReceiver.java
@@ -10,11 +10,11 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.hardware.display.DisplayManager;
-import android.os.IBinder;
 import android.os.UserHandle;
 import android.util.Log;
 import android.view.Display;
 import android.view.Display.HdrCapabilities;
+
 import com.xiaomi.settings.display.ColorModeService;
 import com.xiaomi.settings.turbocharging.TurboChargingService;
 
@@ -24,24 +24,44 @@ public class BootCompletedReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(final Context context, Intent intent) {
-        if (!intent.getAction().equals(Intent.ACTION_BOOT_COMPLETED)) {
-            return;
+        if (DEBUG) Log.i(TAG, "Received intent: " + intent.getAction());
+        switch (intent.getAction()) {
+            case Intent.ACTION_LOCKED_BOOT_COMPLETED:
+                onLockedBootCompleted(context);
+                break;
+            case Intent.ACTION_BOOT_COMPLETED:
+                onBootCompleted(context);
+                break;
         }
-        if (DEBUG)
-            Log.d(TAG, "Received boot completed intent");
+    }
 
-        // Display
-        context.startServiceAsUser(new Intent(context, ColorModeService.class),
-                UserHandle.CURRENT);
+    private static void onLockedBootCompleted(Context context) {
+        overrideHdrTypes(context);
+    }
+
+    private static void onBootCompleted(Context context) {
+        // Start ColorModeService
+        context.startServiceAsUser(new Intent(context, ColorModeService.class), UserHandle.CURRENT);
 
         // Start TurboChargingService
         Intent turboChargingIntent = new Intent(context, TurboChargingService.class);
         context.startService(turboChargingIntent);
+    }
 
-        // Override HDR types to enable Dolby Vision
-        final DisplayManager displayManager = context.getSystemService(DisplayManager.class);
-        displayManager.overrideHdrTypes(Display.DEFAULT_DISPLAY,
-                new int[] {HdrCapabilities.HDR_TYPE_DOLBY_VISION, HdrCapabilities.HDR_TYPE_HDR10,
-                        HdrCapabilities.HDR_TYPE_HLG, HdrCapabilities.HDR_TYPE_HDR10_PLUS});
+    private static void overrideHdrTypes(Context context) {
+        try {
+            final DisplayManager dm = context.getSystemService(DisplayManager.class);
+            if (dm != null) {
+                dm.overrideHdrTypes(Display.DEFAULT_DISPLAY, new int[]{
+                        HdrCapabilities.HDR_TYPE_DOLBY_VISION,
+                        HdrCapabilities.HDR_TYPE_HDR10,
+                        HdrCapabilities.HDR_TYPE_HLG,
+                        HdrCapabilities.HDR_TYPE_HDR10_PLUS
+                });
+                if (DEBUG) Log.i(TAG, "HDR types overridden successfully.");
+            }
+        } catch (Exception e) {
+            Log.e(TAG, "Error overriding HDR types", e);
+        }
     }
 }
