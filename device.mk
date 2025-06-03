4625490470a437a99a7765a8a290470ff4871f45
zenin1504 <kayozzprojects@yandex.com>
2025-06-03 22:58:32 +0000

    duchamp: This is important other roms that have diff path
    Signed-off-by: zenin1504 <kayozzprojects@yandex.com>

--- device.mk
+++ device.mk
@@ -15,6 +15,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/generic_ramdisk.mk)
 # Setup dalvik vm configs
 $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
+# Keys
+-include vendor/lineage-priv/keys/keys.mk
+
 # A/B
 ifneq ($(WITH_GMS),true)
     $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
