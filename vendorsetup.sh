9b4a805636a0477f12f72341518f39e6c9374e22
zenin1504 <kayozzprojects@yandex.com>
2025-06-06 01:57:32 +0000

    duchamp: Drop WPA3 patching and some WIFI commits
    Signed-off-by: zenin1504 <kayozzprojects@yandex.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -8,18 +8,4 @@ git fetch https://github.com/xiaomi-mt6897-duchamp/android_hardware_lineage_comp
 git cherry-pick 9a046ea7e5899adc38ab04fe24eb34859fe4d779
 cd ../../../
 
-# Hardware Legacy Modifications
-echo "Fetching and applying fixes for hardware/libhardware_legacy..."
-cd hardware/libhardware_legacy
-git fetch https://android.googlesource.com/platform/hardware/libhardware_legacy 54bb5d03278152e696c7bff4607278790ac73057
-git cherry-pick 54bb5d03278152e696c7bff4607278790ac73057
-cd ../../
-
-# Add WPA3 fix patch
-echo "Patching WPA3 to work on duchamp device..."
-cd external/wpa_supplicant_8
-git fetch https://github.com/xiaomi-mt6897-duchamp/android_external_wpa_supplicant_8
-git cherry-pick cc88629c6c5c2c2353bf87efef9b5c9c5bf32bee
-cd ../../
-
 echo "All operations completed successfully!"
