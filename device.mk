9a0f529a80286789c97ed976034b3372cd46f62d
zenin1504 <kayozzprojects@yandex.com>
2025-07-04 09:48:20 +0000

    duchamp: Switch to EROFS fully
    The WITH_GMS conditional check for choosing between ext4 and erofs is unnecessary.
    ext4 was only used when WITH_GMS := false to support GAPPS installation.
    Since GAPPS are always bundled, the build can consistently use erofs without any conditionals.
    ext4 is slow and less efficient while erofs is faster and optimized for read-only partitions hence erofs should always be used.
    
    Signed-off-by: zenin1504 <kayozzprojects@yandex.com>

--- device.mk
+++ device.mk
@@ -22,16 +22,12 @@ $(call inherit-product-if-exists, vendor/jdsp/config.mk)
 $(call inherit-product, vendor/mediatek/ims/ims.mk)
 
 # A/B
-ifneq ($(WITH_GMS),true)
-    $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
-    TARGET_RO_FILE_SYSTEM_TYPE := ext4
-else
-    $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/vabc_features.mk)
-    PRODUCT_VIRTUAL_AB_COMPRESSION_METHOD := lz4
-    TARGET_RO_FILE_SYSTEM_TYPE := erofs
-    PRODUCT_VIRTUAL_AB_COW_VERSION := 3
-    PRODUCT_VENDOR_PROPERTIES += ro.virtual_ab.compression.threads=true
-endif
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/vabc_features.mk)
+
+PRODUCT_VIRTUAL_AB_COMPRESSION_METHOD := lz4
+TARGET_RO_FILE_SYSTEM_TYPE := erofs
+PRODUCT_VIRTUAL_AB_COW_VERSION := 3
+PRODUCT_VENDOR_PROPERTIES += ro.virtual_ab.compression.threads=true
 
 PRODUCT_PACKAGES += \
     com.android.hardware.boot \
