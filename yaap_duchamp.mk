db6b7997fe7c3f105087dfa29151df6917a342e5
zenin1504 <kayozzprojects@yandex.com>
2025-07-23 19:46:38 -0700

    duchamp: Update to latest OS2.0.200.0.VNLMIXM
    Signed-off-by: zenin1504 <kayozzprojects@yandex.com>

--- yaap_duchamp.mk
+++ yaap_duchamp.mk
@@ -24,7 +24,7 @@ PRODUCT_SYSTEM_NAME := duchamp_global
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildFingerprint=POCO/duchamp_global/duchamp:15/AP3A.240905.015.A2/OS2.0.103.0.VNLMIXM:user/release-keys \
+    BuildFingerprint=POCO/duchamp_global/duchamp:15/AP3A.240905.015.A2/OS2.0.200.0.VNLMIXM:user/release-keys \
     DeviceProduct=$(PRODUCT_SYSTEM_NAME)
 
 # YAAP stuff's
