ae4b4e69316975a18257cdb2a8286c4272269058
zenin1504 <kayozzprojects@yandex.com>
2025-07-31 13:41:36 -0700

    duchamp: Remove `$(TARGET_RO_FILE_SYSTEM_TYPE)` not needed anymore in pure erofs
    * No need to specify it to $(TARGET_RO_FILE_SYSTEM_TYPE) cause erofs = R/O only
    
    Change-Id: I8a156b3804b04cbfb76be98037d2ab9b
    Signed-off-by: zenin1504 <kayozzprojects@yandex.com>

--- device.mk
+++ device.mk
@@ -35,7 +35,7 @@ PRODUCT_PACKAGES += \
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
     POSTINSTALL_PATH_system=system/bin/otapreopt_script \
-    FILESYSTEM_TYPE_system=$(TARGET_RO_FILE_SYSTEM_TYPE) \
+    FILESYSTEM_TYPE_system=erofs \
     POSTINSTALL_OPTIONAL_system=true
 
 AB_OTA_POSTINSTALL_CONFIG += \
